d812168c8c23aa508fb28fdd16acbd7e9e3e45f5
gnswp21 <gnswp21@gmail.com>
2023-02-26 00:58:00 +0900

    ♻ refactor 사용하지 않는 기능 삭제

--- src/main/java/com/brio/JWTexample/config/SecurityConfiguration.java
+++ src/main/java/com/brio/JWTexample/config/SecurityConfiguration.java
@@ -19,13 +19,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
--- src/main/java/com/brio/JWTexample/member/entity/Member.java
+++ src/main/java/com/brio/JWTexample/member/entity/Member.java
@@ -15,33 +15,10 @@ import java.util.List;
 @Setter
 public class Member {
     Long memberId;
-
     String name;
-
     String email;
-
     String password;
+    List<String> roles = new ArrayList<>();
 
-    MemberStatus memberStatus = MemberStatus.MEMBER_NEW;
-
-    // 멤버 권한
-    private List<String> roles = new ArrayList<>();
-
-    // 연관관계 매핑
-
-    public enum MemberStatus{
-
-        MEMBER_NEW("MEMBER_NEW"),
-        MEMBER_ACTIVE("MEMBER_ACTIVE"),
-        MEMBER_SLEEP("MEMBER_SLEEP"),
-        MEMBER_QUIT("MEMBER_QUIT");
-
-        @Getter
-        String status;
-
-        MemberStatus(String status) {
-            this.status = status;
-        }
-    }
 
 }
